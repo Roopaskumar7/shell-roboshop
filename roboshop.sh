f355145eaa597e9a46dd3ee5feff5c79bdfe5443
Roopaskumar7 <roopaskumar7@gmail.com>
2026-06-23 17:18:36 +0530

    roboshop

---
+++ roboshop.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# shellcheck disable=SC2034
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0222f350150ea711b"
+INSTANCES=("mongob" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "Shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z0946757NTIK8E1MS5MG"
+DOMAIN_NAME="roopaskumar.online"
+
+# shellcheck disable=SC2068
+for instance in ${INSTANCES[@]}
+do
+     
+    # shellcheck disable=SC1073
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0222f350150ea711b --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+    else
+         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        RECORD_NAME="$DOMAIN_NAME"
+    fi
+     echo "$instance IP address: $IP"
+
+
+done
